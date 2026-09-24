d1c0e91fe0ee23d41dd5284b47a9198be1443477
thebag88 <thebag88@gmail.com>
2026-09-24 21:24:14 +0100

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,52 @@
+const quizQuestions = [
+  {
+    question: "The ruins of Urquhart Castle stand on the banks of which famous Scottish loch?",
+    options: {
+      A: "Loch Lomond",
+      B: "Loch Ness",
+      C: "Loch Broom",
+      D: "Loch Maree"
+    },
+    answer: "B"
+  },
+  {
+    question: "In what year was the Beatles album 'Abbey Road' released?",
+    options: {
+      A: "1967",
+      B: "1969",
+      C: "1971",
+      D: "1973"
+    },
+    answer: "B"
+  },
+  {
+    question: "Which actor played James Bond in the 1964 film 'Goldfinger'?",
+    options: {
+      A: "Roger Moore",
+      B: "George Lazenby",
+      C: "Sean Connery",
+      D: "Timothy Dalton"
+    },
+    answer: "C"
+  },
+  {
+    question: "What is the capital city of Australia?",
+    options: {
+      A: "Sydney",
+      B: "Melbourne",
+      C: "Canberra",
+      D: "Brisbane"
+    },
+    answer: "C"
+  },
+  {
+    question: "Which element on the periodic table has the chemical symbol 'Au'?",
+    options: {
+      A: "Silver",
+      B: "Gold",
+      C: "Copper",
+      D: "Aluminum"
+    },
+    answer: "B"
+  }
+];
